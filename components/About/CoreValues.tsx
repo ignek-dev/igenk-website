fd198939b79ce71196cc50486018cb5d721ed6e7
dhaval-sojitra <dhavalsojitra875@gmail.com>
2025-11-25 15:10:40 +0530

    Updated about us page

--- components/About/CoreValues.tsx
+++ components/About/CoreValues.tsx
@@ -42,77 +42,77 @@ const VALUES: CoreValue[] = [
 const CoreValues = () => {
 
     return (
-        <section className="bg-[#F5F5F6] mx-h-[1078px]  py-[3.333vw] relative pb-[64px] global-container">
+        <section className="bg-[#F5F5F6] mx-h-[1078px]  py-[3.333vw] relative global-container">
 
-    {/* Heading */}
-    <div className="flex flex-col items-center">
-        <h2 className="text-[#000000]">
-            Our Core Values
-        </h2>
+            {/* Heading */}
+            <div className="flex flex-col items-center">
+                <h2 className="text-[#000000]">
+                    Our Core Values
+                </h2>
 
-        <p className="text-[0.938vw] leading-[1.563vw] text-[#4B5563] pt-[1.458vw] w-[40.781vw] text-center">
-            Our commitment to integrity, innovation, and teamwork drives us to deliver exceptional Liferay solutions globally.
-        </p>
-    </div>
+                <p className="text-[0.938vw] leading-[1.563vw] text-[#4B5563] pt-[1.458vw] w-[40.781vw] text-center">
+                    Our commitment to integrity, innovation, and teamwork drives us to deliver exceptional Liferay solutions globally.
+                </p>
+            </div>
 
-    {/* Values Section */}
-    <div className=" mt-[1.979vw]">
-        {VALUES.map((v, index) => (
-            <div
-                key={index}
-                className="
+            {/* Values Section */}
+            <div className=" mt-[1.979vw]">
+                {VALUES.map((v, index) => (
+                    <div
+                        key={index}
+                        className="
                     group  h-[9.948vw] pl-[1.979vw] pr-[2.188vw]
-                    bg-[#F5F5F6] pr-[2.188vw]
+                    bg-[#F5F5F6]
                     duration-300 hover:bg-white
                 "
-            >
-                <div className="relative flex h-full items-center px-0">
-                    {/* Number */}
-                    <p
-                        className="text-[1.563vw] text-[#000000] leading-[1.875vw] font-medium"
                     >
-                        {v.number}
-                    </p>
+                        <div className="relative flex h-full items-center px-0">
+                            {/* Number */}
+                            <p
+                                className="text-[1.563vw] text-[#000000] leading-[1.875vw] font-medium"
+                            >
+                                {v.number}
+                            </p>
 
-                    {/* Title */}
-                    <h2
-                        className="
+                            {/* Title */}
+                            <h2
+                                className="
                             truncate pl-[132px] !leading-[3.75vw] !font-normal
                             text-[#000000] 
                             transition-transform duration-300 
                             group-hover:translate-x-[8.906vw]
                             group-hover:pl-[4.135vw]
                         "
-                    >
-                        {v.title}
-                    </h2>
+                            >
+                                {v.title}
+                            </h2>
 
-                    {/* Description */}
-                    <div className="ml-auto">
-                        <p
-                            className="w-[38.177vw] text-right text-[#4B5563] font-normal text-[1.25vw] leading-[1.667vw]"
-                        >
-                            {v.description}
-                        </p>
-                    </div>
+                            {/* Description */}
+                            <div className="ml-auto">
+                                <p
+                                    className="w-[38.177vw] text-right text-[#4B5563] font-normal text-[1.25vw] leading-[1.667vw]"
+                                >
+                                    {v.description}
+                                </p>
+                            </div>
 
-                    {/* Hover image */}
-                    <div
-                        className="
+                            {/* Hover image */}
+                            <div
+                                className="
                             pointer-events-none absolute left-[6.875vw] top-1/2 z-10 
                             h-[6.25vw] w-[6.25vw] -translate-y-1/2 -translate-x-2 
                             overflow-hidden rounded-[0.313vw] opacity-0 
                             transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100
                         "
-                    >
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img src={v.img} alt={v.title} className="h-full w-full object-cover" />
+                            >
+                                {/* eslint-disable-next-line @next/next/no-img-element */}
+                                <img src={v.img} alt={v.title} className="h-full w-full object-cover" />
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
-        ))}
-    </div>
-</section>
+        </section>
 
     );
 };
