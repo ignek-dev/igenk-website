6a52061978aede4a34fa25623c92657654f45ba8
Ridham Kansara <158288081+Ridham-K@users.noreply.github.com>
2025-12-10 11:50:03 +0530

    Refactor BlogCard component layout for improved image scaling on different screens.

--- components/Blogmain/BlogCards.tsx
+++ components/Blogmain/BlogCards.tsx
@@ -27,48 +27,49 @@ export default function BlogCard({ blog }: BlogCardProps) {
     router.push(`/blog/${blog.slug}`);
   };
   return (
-  <div
-  className="cursor-pointer w-auto relative transition flex flex-col"
-  onClick={handleClick}
->
-  {/* Image */}
-  <div className="rounded-[19px] h-[291px] w-[464px] p-1 shadow overflow-hidden w-full">
-    <Image
-      src={blog.image}
-      alt={blog.title}
-      width={500}
-      height={291}
-      className="w-full h-[291px]  rounded-[16px] "
-    />
-  </div>
+    <div className="relative flex w-auto cursor-pointer flex-col transition" onClick={handleClick}>
+      {/* Image */}
+      <div className="h-[15.156vw] w-[24.167vw] overflow-hidden rounded-[0.990vw] p-[0.20vw] shadow">
+        <div className="relative h-full w-full">
+          <Image
+            src={blog.image}
+            alt={blog.title}
+            fill
+            className="rounded-[0.833vw]"
+            sizes="(max-width: 768px) 100vw, 24.167vw"
+          />
+        </div>
+      </div>
 
-  {/* Category */}
-  <div className="flex flex-wrap items-center gap-2 text-gray-500 p18 mt-[0.938vw]">
-    <span className="px-[1.042vw] p18 py-[0.521vw] border-[#D1D5DB] w-[9.5vw] text-center rounded-full border ">
-      {blog.category}
-    </span>
-    <span>•</span>
-    <span>{blog.readTime}</span>
-  </div>
+      {/* Category */}
+      <div className="p18 mt-[0.938vw] flex flex-wrap items-center gap-2 text-gray-500">
+        <span className="p18 w-[9.5vw] rounded-full border border-[#D1D5DB] px-[1.042vw] py-[0.521vw] text-center">
+          {blog.category}
+        </span>
+        <span>•</span>
+        <span>{blog.readTime}</span>
+      </div>
 
-  {/* Title */}
-  <h3 className="text-[1.563vw]! line-clamp-2 h-[4.7vw]  loading-[1.875vw] text-gray-900 mt-[1.042vw] " dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog.title) }} />
+      {/* Title */}
+      <h3
+        className="loading-[1.875vw] mt-[1.042vw] line-clamp-2 h-[4.7vw] text-[1.563vw]! text-gray-900"
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog.title) }}
+      />
 
-  {/* Author Section fixed at bottom */}
-  <div className="flex items-center gap-3  text-lg font-normal text-gray-600 mt-[1.615vw] ">
-    <Image
-      src={blog.authPic}
-      alt={blog.author}
-      width={40}
-      height={40}
-      className="w-10 h-10 object-cover rounded-full border border-gray-200"
-    />
-    <span className=" p20">{blog.author}</span>
-    <span>•</span>
-    <span className="p18 text-[#535862]">{blog.date}</span>
-  </div>
-</div>
-
-);
+      {/* Author Section fixed at bottom */}
+      <div className="mt-[1.615vw] flex items-center gap-3 text-lg font-normal text-gray-600">
+        <Image
+          src={blog.authPic}
+          alt={blog.author}
+          width={40}
+          height={40}
+          className="h-10 w-10 rounded-full border border-gray-200 object-cover"
+        />
+        <span className="p20">{blog.author}</span>
+        <span>•</span>
+        <span className="p18 text-[#535862]">{blog.date}</span>
+      </div>
+    </div>
+  )
 
 }
