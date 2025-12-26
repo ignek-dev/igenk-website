28b0adce26be8cfd1532346b096b0dabbe501ee8
Kalathiya Mehul <kalathiyamehul13899@gmail.com>
2025-12-26 12:15:49 +0530

    Email Added

--- env.mjs
+++ env.mjs
@@ -12,6 +12,8 @@ export const env = createEnv({
     SANITY_DATASET: z.string().optional(),
     SANITY_API_VERSION: z.string().optional(),
     SANITY_READ_TOKEN: z.string().optional(),
+    SENDGRID_API_KEY: z.string().min(1),
+    SENDGRID_FROM_EMAIL: z.string().email(),
   },
   client: {},
   runtimeEnv: {
@@ -20,5 +22,7 @@ export const env = createEnv({
     SANITY_DATASET: process.env.SANITY_DATASET,
     SANITY_API_VERSION: process.env.SANITY_API_VERSION,
     SANITY_READ_TOKEN: process.env.SANITY_READ_TOKEN,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_FROM_EMAIL: process.env.SENDGRID_FROM_EMAIL,
   },
 })
